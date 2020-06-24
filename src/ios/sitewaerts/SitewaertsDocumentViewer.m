b71ace4bb8b6f3fd4be846f0935baa47a12b1ff3
Mohammad Zaman <mohammadz@bggoplan.com>
2020-06-24 11:46:56 -0600

    undo file size commit for not.

--- src/ios/sitewaerts/SitewaertsDocumentViewer.m
+++ src/ios/sitewaerts/SitewaertsDocumentViewer.m
@@ -128,11 +128,11 @@
 	NSURL* baseUrl = [self.webView.request URL];
 #endif
         NSURL* absoluteURL = [[NSURL URLWithString:url relativeToURL:baseUrl] absoluteURL];
-        NSNumber* fileSize = nil;
+        // NSNumber* fileSize = nil;
 
         if ([[NSFileManager defaultManager] fileExistsAtPath:absoluteURL.path]) {
-            [absoluteURL:&fileSize forKey:NSURLFileSizeKey];
-            NSLog(@"[pdfviewer] path: %@, size: %@", absoluteURL.path, fileSize);
+            // [absoluteURL:&fileSize forKey:NSURLFileSizeKey];
+            NSLog(@"[pdfviewer] path: %@", absoluteURL.path);
 
             ReaderDocument *document = [ReaderDocument withDocumentFilePath:absoluteURL.path password:nil];
 
